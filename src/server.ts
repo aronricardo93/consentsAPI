36c83aca8ea58d96c14eaa449f5be0f4774d2050
Aron Ricardo <aronricardo@hotmail.com>
2022-12-10 20:26:39 -0300

    dotenv modified

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
+import * as dotenv from 'dotenv'
 import express from 'express'
-import dotenv from 'dotenv'
 import { Request, Response } from 'express'
 import cors from 'cors'
 import passport from 'passport'
