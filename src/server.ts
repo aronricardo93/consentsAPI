328a92eebdb8c4766f39bb5a378ce35cf9371b4d
Aron Ricardo <aronricardo@hotmail.com>
2022-12-11 15:18:33 -0300

    cors and urlencoded

--- src/server.ts
+++ src/server.ts
@@ -13,8 +13,11 @@ dotenv.config()
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({origin: "*"}))
+
 app.use(passport.initialize())
+
+app.use(express.urlencoded({extended: true}))
 
 app.use('/open-insurance/v1/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
@@ -24,5 +27,5 @@ app.use('/open-insurance/consents/v1',consentRoute)
 app.use((req: Request, res: Response) => {
     res.status(404).json({error: 'Endpoint not found!'})
 })
-
+const doc = '/open-insurance/v1/doc/'
 app.listen(process.env.PORT, () => console.log("Server running..."))
